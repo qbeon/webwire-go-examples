c3fddbabef328a0c4062f85efa7999c080f321bb
Roman Sharkov <roman.sharkov@qbeon.com>
2018-11-29 09:14:08 +0100

    Add pubsub example

---
+++ pubsub/vendor/github.com/qbeon/webwire-go/requestManager/request.go
@@ -0,0 +1,51 @@
+package requestmanager
+
+import (
+	"context"
+
+	webwire "github.com/qbeon/webwire-go"
+	"github.com/qbeon/webwire-go/message"
+	"github.com/qbeon/webwire-go/wwrerr"
+)
+
+// TODO: The request identifier should remain a uint64 until it's converted into
+// the byte array for transmission, this would slightly increase performance
+
+// genericReply is used by the request manager to represent the results of a
+// request that either failed or succeeded
+type genericReply struct {
+	ReplyMsg *message.Message
+	Error    error
+}
+
+// Request represents a request created and tracked by the request manager
+type Request struct {
+	// manager references the RequestManager instance managing this request
+	manager *RequestManager
+
+	// identifier represents the unique identifier of this request
+	Identifier      [8]byte
+	IdentifierBytes []byte
+
+	// Reply represents a channel for asynchronous reply handling
+	Reply chan genericReply
+}
+
+// AwaitReply blocks the calling goroutine
+// until either the reply is fulfilled or failed, the request timed out
+// a user-defined deadline was exceeded or the request was prematurely canceled.
+// The timer is started when AwaitReply is called.
+func (req *Request) AwaitReply(ctx context.Context) (webwire.Reply, error) {
+	// Block until either context canceled (including timeout) or reply received
+	select {
+	case <-ctx.Done():
+		req.manager.deregister(req.Identifier)
+		return nil, wwrerr.TranslateContextError(ctx.Err())
+
+	case rp := <-req.Reply:
+		if rp.Error != nil {
+			return nil, rp.Error
+		}
+		return &reply{msg: rp.ReplyMsg}, nil
+	}
+}
